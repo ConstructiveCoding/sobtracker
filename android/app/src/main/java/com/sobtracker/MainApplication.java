35d4766de609659e7e848a72cdc9fc02e6a0f8b8
Aiden Montgomery <aiden@constructivecoding.com>
2018-10-02 10:02:02 +0100

    Fixed up testing on RN57

--- android/app/src/main/java/com/sobtracker/MainApplication.java
+++ android/app/src/main/java/com/sobtracker/MainApplication.java
@@ -3,8 +3,8 @@ package com.sobtracker;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.beefe.picker.PickerViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
+import com.beefe.picker.PickerViewPackage;
 import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,8 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new PickerViewPackage(),
             new VectorIconsPackage(),
+            new PickerViewPackage(),
             new ReactNativeLocalizationPackage()
       );
     }
